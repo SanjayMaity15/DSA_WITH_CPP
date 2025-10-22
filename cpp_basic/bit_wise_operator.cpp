1358becbbe743329f7efee0124084b7571c19507
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2025-10-22 19:53:36 +0530

    Todays task complete

---
+++ cpp_basic/bit_wise_operator.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    // int a = 5, b = 8;
+
+    // cout << "a&b = " << (a & b)<<endl;
+    // cout << "a|b = " << (a | b)<<endl;
+    // cout << "~a = " << ~a<<endl;
+    // cout << "a^b = " << (a ^ b);
+
+    // decimal to binary
+
+    // int dec = 10;
+    // int result = 0, mul = 1;
+    // while(dec != 0){
+    //     int digit = dec % 2;
+
+    //     result = (digit * mul) + result;
+    //     dec /= 2;
+    //     mul *= 10;
+    // }
+
+    // cout << result;
+
+    // Decimal to ocatal
+
+    // int dec = 10;
+    // int result = 0, mul = 1;
+    // while(dec != 0){
+    //     int digit = dec % 8;
+
+    //     result = (digit * mul) + result;
+    //     dec /= 8;
+    //     mul *= 10;
+    // }
+
+    // cout << result;
+
+    // binary to decimal
+
+    int dec = 1000;
+    int result = 0, mul = 1;
+    while(dec != 0){
+        int digit = dec % 10;
+
+        result = (digit * mul) + result;
+        dec /= 10;
+        mul *= 2;
+    }
+
+    cout << result;
+
+
+}
---
+++ cpp_basic/class_01_.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    //! input and output
+    // int myFavNum, myAge;
+
+    // cout << "Enter your favouritr number and your age : ";
+    // cin >> myFavNum >> myAge;
+    // cout << "My Favourite number is : " << myFavNum <<" and my age is : "<<myAge;
+
+    //! Data types basic
+
+    // int age = 30;
+    // float height = 5.9f;
+    // char grade = 'B';
+    // string name = "John";
+    // bool isStudent = true;
+
+    // cout << "Name: " << name << endl;
+    // cout << "Age: " << age << endl;
+    // cout << "Height: " << height << endl;
+    // cout << "Grade: " << grade << endl;
+    // cout << "Is student? " << isStudent << endl;
+
+    //! operators
+
+    // int a = 10, b = 3;
+    // cout << a + b << endl; // 13
+    // cout << a % b << endl; // 1
+    // a++;
+    // cout << a << endl; // 11
+
+    // int a = 5, b = 10;
+    // cout << (a == b) << endl; // 0 (false)
+    // cout << (a < b) << endl;  // 1 (true)
+
+    // int a = 5, b = 10;
+    // cout << ((a < b) && (b > 0)) << endl; // 1 (true)
+    // cout << ((a > b) || (b < 0)) << endl; // 0 (false)
+    // cout << !(a == 5) << endl;        
+    
+    // 0 (false)
+
+
+    // ! controle statememt
+
+    // I already knew it
+
+    // ! Function
+
+    // I already knew it
+
+    
+
+}
